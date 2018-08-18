737d95a3755f09239916f1dd1ea18106b3bf401a
Ethan Clansky <eclansky@gmail.com>
2018-08-18 10:41:19 -0700

    Got branch release working correctly, on my repo. Target branch naming based on dict works

--- release-tools/release_tool.py
+++ release-tools/release_tool.py
@@ -19,39 +19,33 @@
 # 2) Update release.plist deploytool bump # increment release.plist to next version
 # 3) Generate feature flag report, to tell which FF's changed since last rel.
 
-# PyGithub for talking to GithubAPI; pip install PyGithub
+# WIP
+# Bump release.plist ver using awk/sed/ or py type string tool
+# Gen FF report
 
+# PyGithub for talking to GithubAPI; pip install PyGithub
 from github import Github
 import csv
-
-
-###########################################################
-# Create commandline functionality using argparse
-###########################################################
+import argparse
+import sys
 
 
 ###########################################################
 # Use CSV lib to create arrays of the files
 ###########################################################
-# Load the CSV's into arrays
-# Open the feature flag CSV
-# ff = open("../featureflags/FF.csv")
-# csv_ff = csv.reader(ff)
-#
-# # Open the release info csv_f
-# rif = open("../releng/release_info.csv")
-# csv_rif = csv.reader(rif)
 
 rls_name = []
 rls_ver = []
+releases = {}
 
-# Load into array using csv's DictReader function
+# Load into arrays using csv's DictReader function
 for cell in csv.DictReader(open('../releng/release_info.csv')):
     rls_name.append(cell['rls_name'])
     rls_ver.append(cell['rls_ver'])
 
-print("release names = {}".format(rls_name))
-print("Release versions = {}".format(rls_ver))
+# Create dictionary out of the two arrays for later use and accessiblity.
+releases = dict(zip(rls_name, rls_ver))
+print("Here's our releases dictionary {}".format(releases))
 
 # We'd do something similiar with FF.csv to read in, then modify
 
@@ -67,18 +61,41 @@ tokey = open('auth_token').read()
 gitobj = Github(tokey.rstrip())
 
 # Currently a permissions issue creating a branch in the slack repo,
-# So i'm testing pointing at one of my current repos 
+# So i'm testing pointing at one of my current repos
 # repoName = "br-code-exercise-82044407"
 repoName = "lpic101"
 source_branch = 'master'
 
-# Create new release branch name from rel version
-# # Create the name of the rel branch
-target_branch = 'release_' + rls_name[2] + '.' + rls_ver[1]
+# Create target_branch name from rel version
+# This should print out the values based on whats in hash, not hardcoded.
+
+# Build target branch name using argv[1] and corresponding dict value
+target_branch = 'release_' + sys.argv[1] + releases[sys.argv[1]]
 
 repo = gitobj.get_user().get_repo(repoName)
 sb = repo.get_branch(source_branch)
-repo.create_git_ref(ref='refs/heads/' + target_branch, sha=sb.commit.sha)
+
+
+###########################################################
+# Create commandline functionality using argparse
+###########################################################
+parser = argparse.ArgumentParser()
+
+# Create an argument that ideally takes in the input and releases correct version
+parser.add_argument("version", help="Version to release? ")
+args = parser.parse_args()
+if args.version == 'Apple':
+    repo.create_git_ref(ref='refs/heads/' + target_branch, sha=sb.commit.sha)
+    print("Releasing build {} to new branch".format(target_branch))
+elif args.version == 'Cake':
+    repo.create_git_ref(ref='refs/heads/' + target_branch, sha=sb.commit.sha)
+    print("Releasing build {} to new branch".format(target_branch))
+#elif args.version not matching in hash
+#    print("You must enter one of the following versions to deploy: Apple, Cake")
+
+
+
+
 
 ###########################################################
 # Update release.plist
